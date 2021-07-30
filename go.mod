7db429cf2b3488e7e650d8f31dbc17e5d66f8438
xZero707 <xzero@elite7hackers.net>
2021-07-30 19:37:15 +0200

    Improve logging

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/freshman-tech/file-upload
 
 go 1.15
+
+require github.com/gorilla/handlers v1.5.1
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/handlers"
 	"io"
 	"log"
 	"net/http"
@@ -15,18 +16,21 @@ const UPLOAD_PATH="/storage"
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
+		log.Println("Error: Method not allowed")
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	// 32 MB is the default used by FormFile
 	if err := r.ParseMultipartForm(32 << 20); err != nil {
+		log.Println(err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
     maxUploadSize, err := strconv.ParseInt(os.Getenv("MAX_UPLOAD_SIZE"), 10, 64)
 	if err != nil {
+		log.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -42,6 +46,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 		srcFile, err := fileHeader.Open()
 		if err != nil {
+			log.Println(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -50,18 +55,21 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 		_, err = srcFile.Seek(0, io.SeekStart)
 		if err != nil {
+			log.Println(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		err = os.MkdirAll(UPLOAD_PATH, os.ModePerm)
 		if err != nil {
+			log.Println(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		destFile, err := os.Create(fmt.Sprintf("%s/%d%s", UPLOAD_PATH, time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
 		if err != nil {
+			log.Println(err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
@@ -70,9 +78,12 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 		_, err = io.Copy(destFile, srcFile)
 		if err != nil {
+			log.Println(err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+
+		log.Printf("Upload complete to %s", UPLOAD_PATH)
 	}
 
 	fmt.Fprintf(w, "Upload successful")
@@ -82,7 +93,9 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/upload", uploadHandler)
 
-	if err := http.ListenAndServe(":4500", mux); err != nil {
+    log.Println("Server started")
+    log.Printf("Upload size limited to %s bytes", os.Getenv("MAX_UPLOAD_SIZE"))
+	if err := http.ListenAndServe(":4500", handlers.LoggingHandler(os.Stdout, mux)); err != nil {
 		log.Fatal(err)
 	}
 }
